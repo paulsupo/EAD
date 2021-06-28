711580b256909da726b9b4673adaae0d7dfd3848
paulsupo <42578857+paulsupo@users.noreply.github.com>
2021-06-27 23:53:32 -0500

    Update main.cpp

--- lista_ver/main.cpp
+++ lista_ver/main.cpp
@@ -23,37 +23,26 @@ class LE
 public:
     void print();
     bool find(int v, nodo*& pos);
-    bool find2(int posicion, int version, int campo);
-    void add(nodo *temp, int campo, int v);
+    nodo* find2(int posicion, int version, int campo);
+    void add(int v);
+    void add2(nodo* temp, int campo, int valor, int version);
+    void add_def(int opcion);
     void del(int v);
     ~LE();
 };
 
 void LE::print()
 {
+    int i;
     cout << "Head->";
-    for (nodo* p = head; p; p = p->next)
-        cout << p->valor << "->";
+    for (nodo* p = head; p; p = p->next) {
+        if (p->vect.size() > 0)
+            i = p->vect.size() - 1;
+        cout << p->vect[i].first << "->";
+    }
+        
     cout << "NULL" << endl;
 }
-
-bool LE::find2(int posicion, int version, int campo)
-{
-    bool encontrado = false;
-    nodo* pos = nullptr;
-    for (nodo* p = head, int i=0; p && i< posicion; p = p->next)
-    {
-        if (p->valor == valor)
-        {
-            encontrado = true;
-            break;
-        }
-        pos = p;
-        //cout<<pos->valor<<endl;
-    }
-
-}
-
 
 bool LE::find(int v, nodo*& pos)
 {
@@ -73,6 +62,23 @@ bool LE::find(int v, nodo*& pos)
     return encontrado;
 }
 
+
+nodo* LE::find2(int posicion, int version, int campo)
+{
+    bool encontrado = false;
+    nodo* pos = nullptr;
+    nodo* p;
+    int i;
+    for (p = head, i = 0; p && i < posicion; p = p->next,i++)
+    {
+        pos = p;
+        
+    }
+    
+    return p;
+}
+
+
 void LE::add(int v)
 {
     nodo* pos_ant;
@@ -87,6 +93,67 @@ void LE::add(int v)
             head = new nodo(v, head);
         }
     }
+}
+
+void LE::add2(nodo* temp, int campo, int valor, int version) {
+    ///=valor, 2=puntero
+    int opcion_;
+    if (campo == 1) {
+        cout << "1. Conservar la verion y cambiar el valor" << endl;
+        cout << "2. Crear una nueva version" << endl;
+        cin >> opcion_;
+        if (opcion_ == 1) {
+            cout << "Valor a cambiar: " << temp->vect[version].first << endl;
+            temp->vect[version].first = valor;
+        }
+        
+        if (opcion_ == 2) {
+            nodo* temp_ = temp->next;
+            nodo* nuevo = new nodo(valor, temp_);
+            
+            temp->vect.push_back(make_pair(valor, nuevo));
+            nuevo->next = temp_;
+        }
+    }
+
+    if (campo == 2) {
+        cout << "Se creara una nueva version del nodo" << endl;
+        if (temp->next != nullptr) {
+            nodo* temp2 = temp->next;
+            int temp__ = temp->vect[version].first;
+            nodo* nuevo = new nodo(valor, temp2);
+            temp->next = nuevo;
+            temp->vect.push_back(make_pair(temp__, nuevo));
+            nuevo->next = temp2;
+        }
+    }
+}
+
+void LE::add_def(int opcion) {
+    if (opcion == 1) {
+        int valor1;
+        cout << "Digite un numero" << endl;
+        cin >> valor1;
+        add(valor1);
+    }
+
+    if (opcion == 2) {
+        int posi, campo, num, version;
+        cout << "Ingrese la posicion del nodo a modificar" << endl;
+        cin >> posi;
+        cout << endl << "Ingrese la version a modificar" << endl;
+        cin >> version;
+        cout << endl << "Ingrese el campo del nodo a modificar (1=valor, 2=puntero)" << endl;
+        cin >> campo;
+        cout << endl << "Digite el numero" << endl;
+        cin >> num;
+
+        nodo* temp = find2(posi,version, campo);
+        cout << "NOdo en que estamos: " << temp->vect[version].first << endl;
+        add2(temp, campo, num, version);
+       
+    }
+
 }
 
 void LE::del(int v)
@@ -120,7 +187,7 @@ LE lista;
 
 void menu() {
     int opcion;
-    int dato, datoaeliminar;
+    int dato, opcion_insertar, datoaeliminar;
     do {
         cout << "\t         MENU" << endl;
         cout << "1. Insertar elemento en la lista" << endl;
@@ -131,9 +198,10 @@ void menu() {
         cin >> opcion;
 
         switch (opcion) {
-        case 1: cout << "\nDigite un numero: ";
+        case 1: cout << "1. Desea poner el elemento al final de la lista"<<endl;
+            cout << "2. Desea modificar un nodo en la lista" << endl;
             cin >> dato;
-            lista.add(dato);
+            lista.add_def(dato);
             system("pause");
             break;
         case 2: cout << "\nEscriba el dato a eliminar: ";
